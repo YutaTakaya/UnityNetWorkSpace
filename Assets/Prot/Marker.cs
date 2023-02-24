85705dd8fd2c14c59fb0863eef00e951fa5d18c8
YutaTakaya <yutapon05@gmail.com>
2023-02-24 19:24:52 +0900

    サンプルシーンの作成

---
+++ Assets/Prot/Marker.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+    private float startTime;
+    [SerializeField] float markInterval = 100.0f;
+    [SerializeField] GameObject markerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime >= markInterval)
+        {
+            GameObject obj = Instantiate(markerPrefab);
+            obj.transform.position = this.transform.position;
+
+            HumanPoseHandler pose;
+            this.GetComponent<HumanPoseHandler>().GetHumanPose(ref pose);
+            startTime += markInterval;
+        }
+    }
+}
